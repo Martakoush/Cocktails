d318e6b90489417e64e3a1c381b3da5def384228
Martakoush <martakoush.mhd@gmail.com>
2025-07-03 16:52:09 +0300

    implement the third section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
         .to('.left-leaf',{y:-200},0)
         
         const startValue = isMobile ? 'top 50%':'center 60%';
-        const endValue = isMobile? '120% top':'bottom top';
+        const endValue = isMobile? '180% top':'bottom top';
         
         let tl = gsap.timeline({
         scrollTrigger: {
@@ -55,7 +55,6 @@ const Hero = () => {
         end: endValue,
         scrub: true,
         pin: true,
-        markers:true
         },
     });
 
